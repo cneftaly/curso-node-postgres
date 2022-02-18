13d3cfe96c6a202d2e2f80a20f6a30add22c15b0
Carlos Sosa <carlosneftaly@hotmail.com>
2022-02-18 17:01:34 -0600

    Clase 22 07_15

--- db/models/order.model.js
+++ db/models/order.model.js
@@ -35,6 +35,12 @@ class Order extends Model {
     this.belongsTo(models.Customer, {
       as: 'customer'
     });
+    this.belongsToMany(models.Product, {
+      as: 'items',
+      through: models.OrderProduct,
+      foreignKey: 'order_id',
+      otherKey: 'productId'
+    })
   }
 
   static config(sequelize) {
