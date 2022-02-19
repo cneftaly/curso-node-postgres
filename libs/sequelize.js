1b81724607fc9f787d8c076de3924e4d861d5f9c
Carlos Sosa <carlosneftaly@hotmail.com>
2022-02-19 12:59:06 -0600

    Previous to deployment

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,16 +2,18 @@ const { Sequelize } = require('sequelize');
 const { config } = require('../config/config');
 const setupModels = require('../db/models');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const options = {
+  dialect: 'postgres',
+  logging: config.isProd ? false : console.log,
+};
 
-const sequelize = new Sequelize(URI,
-  {
-    dialect: 'postgres',
-    logging: console.log
+if (config.isProd) {
+  options.ssl = {
+    rejectUnauthorized: false
   }
-  );
+}
+
+const sequelize = new Sequelize(config.dbUrl, options);
 
 setupModels(sequelize);
 
